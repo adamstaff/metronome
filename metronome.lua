4ca1d99d3f535f5eaf1debfbf1bfc2a3b9c313f3
Adam Staff <52344911+adamstaff@users.noreply.github.com>
2025-09-14 12:02:30 +0100

    Update metronome.lua
    Added params to change metronome pitch
    some tidying

--- metronome.lua
+++ metronome.lua
@@ -29,19 +29,19 @@ function ticker()
       beatScreen = 15
       count.number = math.floor((clockPosition / count.barlength) * params:get("upperNumber") + 1)
       engine.amp(1)
-      engine.hz(100)
+      engine.hz(beatFreq)
     else if math.floor(clockPosition % count.subBeatLength) == 0 then -- we're on a subcount
       --play a big sound
       beatScreen = 6
       count.number = math.floor((clockPosition / count.barlength) * params:get("upperNumber") + 1)
       engine.amp(0.5)
-      engine.hz(100)
+      engine.hz(beatFreq)
     else if math.floor(clockPosition % count.beatLength) == 0 then -- we're on a small beat
       -- play a small sound
       beatScreen = 3
       count.number = math.floor((clockPosition / count.barlength) * params:get("upperNumber") + 1)
       engine.amp(0.5)
-      engine.hz(200)
+      engine.hz(subBeatFreq)
     else
       --anything here?
       count.bigBeat = false
@@ -68,9 +68,6 @@ function init()
   redraw_clock_id = clock.run(redraw_clock) --add these for other clocks so we can kill them at the end
   
   --variables
- -- upperNumber = 0
---  lowerNumber = 0
---  subcount = 0
   count = {}
   count.beatLength = 0
   count.subBeatLength = 0
@@ -84,6 +81,8 @@ function init()
   count.bigBeat = false
   count.smallBeat = false
   count.number = 1
+  beatFreq = 110
+  subBeatFreq = 220
   --end variables
   
   -- start params
@@ -100,12 +99,21 @@ function init()
   params:set_action("subcount", function()
     count.recalculate()
   end)
+  params:add_number("beat_note", "beat note", 0, 127, 36,
+    function(param) return MusicUtil.note_num_to_name(param:get(), true) end
+  } 
+  params:set_action("beat_note", function()
+    beatFreq = MusicUtil.note_num_to_freq(params:get("beat_note"))
+  ) 
+  params:add_number("sub_beat_note", "sub beat note", 0, 127, 48,
+    function(param) return MusicUtil.note_num_to_name(param:get(), true) end
+  }
+  params:set_action("sub_beat_note", function()
+    subBeatFreq = MusicUtil.note_num_to_freq(params:get("sub_beat_note"))
+  )
+  
   params:bang() -- set defaults using above params
   --end params
-
-  --could use these to make the lower number start with powers of 2?
-  --segmentLength = 6 -- index to read from resolutions, i.e. resolutions[segmentLength]
-  --resolutions = {1,2,3,4,6,8,12,16,24,32,48,64,96,128,192}
 
   --drawing stuff
   beatScreen = 0 --screen level: set to 15 when the metronome pings to flash the screen
@@ -122,8 +130,7 @@ function init()
   --file = {} --add samples: could be used to load bing samples
   engine.release(0.1)
 
-  g:all(0)
-  
+  g:all(0)  --clear grid
   screenDirty = true -- make sure we draw screen straight away
 
 end
@@ -139,11 +146,8 @@ function drawView()
   --draw white or black text
   screen.level(15 - beatScreen)
   
-  --what view we in?
   screen.move(0,5)
   screen.text(clock.get_tempo())
-  
-  -- could we highlight stuff depending what view we're in?
   
   --time signature, big nice text
   screen.move(96,49)
@@ -297,11 +301,6 @@ function key(k, z)
         clock.run(ticker) end
     end
   end
-  
---  if k == 3 and z == 1 then -- togle view
---    if mainView then mainView = false
---    else mainView = true end
---  end
 
   screen_dirty = true
 
